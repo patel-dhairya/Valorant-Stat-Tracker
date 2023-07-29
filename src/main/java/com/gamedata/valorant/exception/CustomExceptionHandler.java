b01f20224096f8709e34e5878fb87ae29dc60693
Dhairya Patel <p5d7@proton.me>
2023-07-29 21:03:16 +0530

    Implementation of slf4j logger
    >> Summary
    > This commit adds logger class from slf4j to log errors.
    > This commit also separate dev and local environments for temporary time. Might revert back to single environment once I finish testing the logger class.

--- src/main/java/com/gamedata/valorant/exception/CustomExceptionHandler.java
+++ src/main/java/com/gamedata/valorant/exception/CustomExceptionHandler.java
@@ -2,6 +2,8 @@
 
 package com.gamedata.valorant.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -15,12 +17,16 @@ import java.util.List;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<ErrorModel>> handleDataValidation(MethodArgumentNotValidException mav){
         List<ErrorModel> errorModelList = new ArrayList<>();
         ErrorModel errorModel = null;
         List<FieldError> fieldErrorList = mav.getBindingResult().getFieldErrors();
         for (FieldError fieldError: fieldErrorList){
+            logger.debug("Inside field validation: {} - {}", fieldError.getField(), fieldError.getDefaultMessage());
+            logger.info("Inside field validation: {} - {}", fieldError.getField(), fieldError.getDefaultMessage());
             errorModel = new ErrorModel();
             errorModel.setCode(fieldError.getField());
             errorModel.setMessage(fieldError.getDefaultMessage());
@@ -33,6 +39,10 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException businessException ){
+        for (ErrorModel errorModel: businessException.getErrorModelList()) {
+            logger.debug("Business Exception : {} - {}", errorModel.getCode(), errorModel.getMessage());
+            logger.info("Business Exception: {} - {}", errorModel.getCode(), errorModel.getMessage());
+        }
         return new ResponseEntity<List<ErrorModel>>(businessException.getErrorModelList(), HttpStatus.BAD_REQUEST);
     }
 }
